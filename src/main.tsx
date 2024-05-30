d5f3ae15b5510b7bba74b50aa244b85811ef538d
Karl <karl_dilma@hotmail.com>
2024-05-30 20:01:46 +1000

    feat: emotion themes

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Theme, ThemeProvider } from "@mui/material/styles";
+import { Global } from "@emotion/react";
 
 import App from "./App";
-import "./design-system/index.css";
+
+import { global, theme } from "./design-system/css/";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Global styles={(theme) => global(theme as Theme)} />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
